e0ca09abc83d508a112bf713e3a010e04122ea0e
npwolf <dev@wolfplusplus.com>
2022-03-08 16:52:58 -0800

    Connecting client and server

--- client/src/components/CitiesForm.vue
+++ client/src/components/CitiesForm.vue
@@ -1,48 +1,58 @@
 <template>
   <div class="cities-form">
     <div class="form-group">
-    <form class="form-inline" @submit.prevent="onSubmit">
+      <form class="form-inline" @submit.prevent="onSubmit">
         <label for="citiesBox">Enter Cities</label>
-        <textarea class="form-control" id="citiesBox" rows="15" v-bind:value="citiesList"></textarea>
-        <button type="submit" class="btn btn-success btn-sm">Get Weather</button>
-    </form>
+        <textarea
+          class="form-control"
+          id="citiesBox"
+          rows="15"
+          v-bind:value="citiesList"
+        ></textarea>
+        <button type="submit" class="btn btn-success btn-sm">
+          Get Weather
+        </button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-  name: 'CitiesForm',
+  name: "CitiesForm",
   data() {
-      return {
-          citiesList: "Mesa, AZ",
-          weatherData: [
-          { 
-            state: 'AZ',
-            city: 'Mesa',
-            january: { 
-                high: 60, 
-                low: 45
-                },
-          },
-          {
-            state: 'MT',
-            city: 'Bozeman',
-            january: { 
-                high: 45, 
-                low: 20
-                },
-          },  
-      ],  
-      }
+    return {
+      citiesList: "Mesa, AZ",
+      server_base_url: "",
+    };
   },
   methods: {
-      onSubmit() {
-          this.$emit('cities-changed', this.weatherData)
-      }
+    onSubmit() {
+      const url = `${this.server_base_url}/weather`;
+      console.log(url)
+      axios
+        .get(url)
+        .then((res) => {
+          this.$emit('cities-changed', res.data)
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
   },
-  emits: ['cities-changed']
-}
+  emits: ["cities-changed"],
+  created() {
+    if (typeof process.env.SERVER_BASE_URL !== 'undefined') {
+      this.server_base_url = process.env.SERVER_BASE_URL;
+    } else {
+      console.log("Using local dev url. Set SERVER_BASE_URL if different.")
+      this.server_base_url = "http://localhost:8000";
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -51,11 +61,11 @@ export default {
   padding: 1.5rem;
   margin-right: 0;
   margin-left: 0;
-  border-width: .2rem;
+  border-width: 0.2rem;
   font-size: 1rem;
   font-weight: 400;
   line-height: 1.5;
   color: #212529;
-  text-align: left;  
+  text-align: left;
 }
 </style>
--- server/main.py
+++ server/main.py
@@ -1,14 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:8080",
+]
+
+if "CLIENT_URL" in os.environ:
+    origins.append(os.environ["CLIENT_URL"])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/weather")
 async def weather():
     return [
           { 
             "state": 'AZ',
-            "city": 'Mesa',
+            "city": 'Phoenix',
             "january": { 
                 "high": 60, 
                 "low": 45
