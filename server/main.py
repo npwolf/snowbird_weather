7cb352235a6a5865e5630370e18efc8293240094
npwolf <dev@wolfplusplus.com>
2022-03-09 07:23:57 -0800

    No local dev urls when CLIENT_URL set

--- server/main.py
+++ server/main.py
@@ -5,12 +5,13 @@ import os
 
 app = FastAPI()
 
-origins = [
-    "http://localhost:8080",
-]
-
 if "CLIENT_URL" in os.environ:
-    origins.append(os.environ["CLIENT_URL"])
+    origins = [os.environ["CLIENT_URL"])]
+else:
+    # Local development URLs
+    origins = [
+        "http://localhost:8080",
+    ]
 
 app.add_middleware(
     CORSMiddleware,
@@ -22,22 +23,18 @@ app.add_middleware(
 
 fastapi_logger.info(f"CORS origins: {origins}")
 
+
 @app.get("/weather")
 async def weather():
     return [
-          { 
-            "state": 'AZ',
-            "city": 'Phoenix',
-            "january": { 
-                "high": 60, 
-                "low": 45
-                },
-          },
-          {
-            "state": 'MT',
-            "city": 'Bozeman',
-            "january": { 
-                "high": 45, 
-                "low": 20
-                },
-          },]
+        {
+            "state": "AZ",
+            "city": "Phoenix",
+            "january": {"high": 60, "low": 45},
+        },
+        {
+            "state": "MT",
+            "city": "Bozeman",
+            "january": {"high": 45, "low": 20},
+        },
+    ]
